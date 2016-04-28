baf52bce7c4a04da1326ede4eb8b042963b1c4c5
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-04-28 23:38:56 +0300

    cleaning multiple

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   has_many :documents
-  attr_accessor :image_base
+
   attr_accessor :document_data
-  # before_validation :parse_image
+
 
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment :picture, presence: true
@@ -16,11 +16,5 @@ class Item < ApplicationRecord
     end
   end
 
-  private
-  def parse_image
 
-    image = Paperclip.io_adapters.for(image_base)
-    image.original_filename = "image.jpg"
-    self.picture = image
-  end
 end
