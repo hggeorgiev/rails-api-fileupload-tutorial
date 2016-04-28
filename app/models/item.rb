07c11528d8d7a89cfcb8a5e3d6910e79087fa331
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-04-28 15:54:27 +0300

    adding base64

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
