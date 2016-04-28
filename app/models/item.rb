1b5916297a8a338816e0ac7c26f4c3a5f084b2d7
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-04-28 16:12:02 +0300

    base64

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
